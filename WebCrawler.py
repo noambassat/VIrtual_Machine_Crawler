dd26db7716923e93e777a11522887de0290f9399
noambassat <noambassat92@gmail.com>
2022-05-29 15:14:51 +0300

    + dates

--- WebCrawler.py
+++ WebCrawler.py
@@ -16,26 +16,29 @@ from datetime import timedelta
 
 # iterate days
 
-start_date = datetime.date(2022, 5, 21)
+start_date = datetime.date(2022, 1, 1)
 end_date = start_date + timedelta(days=1)
-stop = datetime.date(2022, 5, 23)
+stop = datetime.date(2022, 1, 3)
 single_date = start_date
 while single_date != stop:
     end_date = single_date + timedelta(days=1)
     print("start: ", single_date, "end : ", end_date)
     single_date += timedelta(days=1)
-
+print()
+def date_to_string(date):
+    return str(date.day) + '/' + str(date.month) + '/' + str(date.year)
 driver = webdriver.Chrome(executable_path='C:/Users/Noam/Desktop/Courts Project/chromedriver.exe')
-driver.get("https://www.supreme.court.gov.il/Pages/HomePage.aspx")
+driver.get("https://supreme.court.gov.il/Pages/HomePage.aspx")
+time.sleep(2)
 driver.find_element_by_xpath("/html/body/form/div[3]/div[1]/div/header/div[2]/div/div[2]/div/div[2]/a[5]/span").click()
 time.sleep(1)
 
 select = Select(driver.find_element(by = By.XPATH, value = '//select[@title="טווח זמנים"]'))
 select.select_by_index(7)
 #start date
-driver.find_element_by_xpath('/html/body/form/div[3]/div[3]/div/div/form/div/div/div[2]/div[5]/div[2]/div/div/input').send_keys(start_date)
+driver.find_element_by_xpath('/html/body/form/div[3]/div[3]/div/div/form/div/div/div[2]/div[5]/div[2]/div/div/input').send_keys(date_to_string(single_date))
 #end date
-driver.find_element_by_xpath('/html/body/form/div[3]/div[3]/div/div/form/div/div/div[2]/div[5]/div[3]/div/div/input').send_keys(end_date)
+driver.find_element_by_xpath('/html/body/form/div[3]/div[3]/div/div/form/div/div/div[2]/div[5]/div[3]/div/div/input').send_keys(date_to_string(end_date))
 # search
 driver.find_element_by_xpath('/html/body/form/div[3]/div[3]/div/div/form/div/div/div[2]/div[7]/div[2]/button').click()
 time.sleep(3)
