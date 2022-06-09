c098f01afaf761b6f80373aceeef98a7fb141841
noambassat <noambassat92@gmail.com>
2022-06-09 16:32:46 +0300

    + src

--- CrawlUrls.py
+++ CrawlUrls.py
@@ -1,15 +1,3 @@
-###
-
-# TO DO LIST
-
-# location_once_scrolled_into_view
-
-# GET SRC
-
-
-
-
-
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -20,10 +8,19 @@ from lxml import html
 import requests
 # import urllib2
 from selenium.webdriver.common.keys import Keys
+###
+import re
+
+# TO DO LIST
+
+# location_once_scrolled_into_view
+
+# GET SRC
+
 
 URLS = pd.DataFrame(columns=['URL'])
 driver = webdriver.Chrome(executable_path='C:/Users/Noam/Desktop/Courts Project/chromedriver.exe')
-driver.get("https://supreme.court.gov.il/Pages/SearchJudgments.aspx?&OpenYearDate=null&CaseNumber=null&DateType=2&SearchPeriod=null&COpenDate=30/05/2022&CEndDate=31/05/2022&freeText=null&Importance=null")
+# driver.get("https://supreme.court.gov.il/Pages/SearchJudgments.aspx?&OpenYearDate=null&CaseNumber=null&DateType=2&SearchPeriod=null&COpenDate=30/05/2022&CEndDate=31/05/2022&freeText=null&Importance=null")
 
 
 
@@ -36,21 +33,19 @@ def get_urls(driver, start_date, end_date):
           + CaseNUMBER +\
           "&DateType=2&SearchPeriod=null&COpenDate=30/05/2022&CEndDate=31/05/2022&freeText=null&Importance=null"
 
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    #ng-scope active
-    #//*[@id="row_0"]/div[2]/a
-    card = soup.find("div",{"class":"Layout-HomePage"}).find(
-        "div",{"id":"MSOZoneCell_WebPartWPQ2"}).find(
-        "div",{"class":"ms-webpart-chrome ms-webpart-chrome-vertical ms-webpart-chrome-fullWidth"}).find(
-        "div",{"id":"ctl00_ctl40_g_664c5230_fd36_4083_9f79_25d174af43f8"}).find(
-        "div",{"elad_wpid":"ctl00$ctl40$g_664c5230_fd36_4083_9f79_25d174af43f8"}).find("iframe",{"id":"serviceFram"})
-    src = card['src']
+    # soup = BeautifulSoup(driver.page_source, 'html.parser')
+    # #ng-scope active
+    # #//*[@id="row_0"]/div[2]/a
+    # card = soup.find("div",{"class":"Layout-HomePage"}).find(
+    #     "div",{"id":"MSOZoneCell_WebPartWPQ2"}).find(
+    #     "div",{"class":"ms-webpart-chrome ms-webpart-chrome-vertical ms-webpart-chrome-fullWidth"}).find(
+    #     "div",{"id":"ctl00_ctl40_g_664c5230_fd36_4083_9f79_25d174af43f8"}).find(
+    #     "div",{"elad_wpid":"ctl00$ctl40$g_664c5230_fd36_4083_9f79_25d174af43f8"}).find("iframe",{"id":"serviceFram"})
+    # src = card['src']
+    src1 = "https://supremedecisions.court.gov.il/Verdicts/Results/1/null/null/null/2/null/30-05-2022/31-05-2022/null"
 
-
-
-
-    driverf = webdriver.Chrome(executable_path='C:/Users/Noam/Desktop/Courts Project/chromedriver.exe')
-    driverf.get(src)
+    src = 'https://supremedecisions.court.gov.il/Verdicts/Results/1/null/null/null/2/null/' + start_date.replace('/','-') + '/' + end_date.replace('/','-') + '/null'
+    driver.get(src)
     #
     # scrollable_element = driverf.find_element_by_class_name("results-listing")
 
@@ -75,9 +70,9 @@ def get_urls(driver, start_date, end_date):
     #
 
 
-    elements = driverf.find_elements_by_class_name('ng-scope')
+    elements = driver.find_elements_by_class_name('ng-scope')
     for element in elements:
-        soup = BeautifulSoup(driverf.page_source, 'html.parser')
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
         soup = soup.findAll('a',{'title':'הצג תיק'})
         print(len(soup))
         # for i, s in enumerate(soup): print(i, ":   ", s)
