79b4f309ec829a631bb977c3aa77b0cae05f7f27
noambassat <noambassat92@gmail.com>
2022-06-12 14:18:26 +0300

    + scroll down

--- CrawlUrls.py
+++ CrawlUrls.py
@@ -2,19 +2,20 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+import main
 
 
 
 def get_src(start_date, end_date): return 'https://supremedecisions.court.gov.il/Verdicts/Results/1/null/null/null/2/null/' + start_date.replace('/','-') + '/' + end_date.replace('/','-') + '/null'
 
-def Get_Number_Of_Cases():
+def Get_Number_Of_Cases(driver):
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     txt = soup.find('p', {'class': 'ng-binding'}).text
     return [int(s) for s in txt.split() if s.isdigit()][0]
 
 
 
-def scroll_down(Number_Of_Cases):
+def scroll_down(driver, Number_Of_Cases):
     driver.maximize_window()  # For maximizing window
     driver.implicitly_wait(5)  # gives an implicit wait for 5 seconds
     Counter = 99
@@ -25,7 +26,7 @@ def scroll_down(Number_Of_Cases):
         Counter +=100
 
 
-def Get_Cases_Names():
+def Get_Cases_Names(driver,):
 
     elements = driver.find_elements_by_class_name('ng-scope')
 
@@ -36,22 +37,32 @@ def Get_Cases_Names():
     print(len(soup) , " Cases were found!")
     return [s.text for s in soup]
 
-driver = webdriver.Chrome(executable_path='C:/Users/Noam/Desktop/Courts Project/chromedriver.exe')
 
-start = '30/05/2022'
-end =  '31/05/2022'
-src = get_src(start,end)
+def Get_URLS(Cases):
+    for case in Cases:
+        for word in case.split(): print(word)
+    return []
 
-driver.get(src)
+Get_URLS(['רע"א 3484/22','ע"א 3339/22'])
 
+###################################################################################################
 
-time.sleep(1)
-
-
-Number = Get_Number_Of_Cases()
-scroll_down(Number)
-Cases = Get_Cases_Names()
-
-df =pd.DataFrame(Cases,columns=[start])
-df.to_csv('Cases_Name.csv')
-print(df.head())
+#
+#
+# driver = webdriver.Chrome(executable_path='C:/Users/Noam/Desktop/Courts Project/chromedriver.exe')
+#
+# start = '30/05/2022'
+# end =  '31/05/2022'
+# src = get_src(start, end)
+#
+# driver.get(src)
+#
+# time.sleep(1)
+#
+# Number = Get_Number_Of_Cases()
+# scroll_down(Number)
+# Cases = Get_Cases_Names()
+#
+# df =pd.DataFrame(Cases,columns=[start])
+# df.to_csv('Cases_Name.csv')
+# print(df.head())
--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-# This is a sample Python script.
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+import main
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import CrawlUrls
+from CrawlUrls import get_src, Get_Cases_Names, Get_Number_Of_Cases, scroll_down
 
+
+driver = webdriver.Chrome(executable_path='C:/Users/Noam/Desktop/Courts Project/chromedriver.exe')
+
+start = '30/05/2022'
+end =  '31/05/2022'
+src = get_src(start, end)
+
+driver.get(src)
+
+time.sleep(1)
+
+Number = Get_Number_Of_Cases(driver)
+scroll_down(driver, Number)
+Cases = Get_Cases_Names(driver)
+
+df =pd.DataFrame(Cases,columns=[start])
+df.to_csv('Cases_Name.csv')
+print(df.head())
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
