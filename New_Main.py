4c973061111c0eeb1843a9bae62096b5adbfeb7d
noambassat <noambassat92@gmail.com>
2022-10-27 18:44:01 +0300

    New_Crawler

--- New_Main.py
+++ New_Main.py
@@ -18,7 +18,6 @@ import Save_As_Json
 
 """
 יהונתן - 
-איך לבצע את הלוג במקרה של כשל - שישמר תוך כדי הריצה ולא רק בסיומה?1. 
 איך לייצר  משתנים גלובליים לכל התוכנה?2. 
 איך אפשר לערוך קבצי CSV 3.
 בUBUNTU ???
@@ -49,7 +48,6 @@ options = Options()
 PROXY = "5.79.66.2:13081"
 options.add_argument('--proxy-server=%s' % PROXY)
 
-
 """"
 IMPORTANT LOGICAL LINKS:
     src = "https://elyon2.court.gov.il/Scripts9/mgrqispi93.dll?Appname=eScourt&Prgname=GetFileDetails_for_new_site&Arguments=-N" \
@@ -65,16 +63,13 @@ Full_Log_Dict = {}
 Logs_list = []
 Years_and_Nums = {2011:9775} # { year_num : num_of_cases }
 
-
 driver = webdriver.Chrome(exe_path, options=options)
 
 for year in Years_and_Nums.keys(): # CURR -> 2011 ONLY
     counter, STOP = 3,0  # The Continuous number of each year
     while(counter<Years_and_Nums[year] and STOP < 5): # While the crawler didn't reach the case's limit number yet. 5 is the max errors that can be thrown.
         try:
-            for case_log in Logs_list:
-                # append data frame to CSV file
-                df.to_csv('Logs_DF.csv', mode='a', index=False, header=False)
+            df.to_csv('Logs_DF.csv', mode='a', index=False, header=False)
         except UnboundLocalError:
             pass
 
@@ -82,27 +77,31 @@ for year in Years_and_Nums.keys(): # CURR -> 2011 ONLY
         Case_Link = Crawl_Data.get_url(year, counter)
         curr_case["קישור לתיק"] = Case_Link
 
+        ########################## רשימת השגיאה - להכניס גם לטבלת הלוג
         try:
             driver.get(Case_Link)
         except WebDriverException as wde:
             print(str(wde),"\n",Case_Link)
 
+        ##########################
 
         ####
         WebDriverWait(driver,3)
         soup = BeautifulSoup(driver.page_source, 'html.parser')
-        if(counter>Years_and_Nums[year]):
-            try:
-                if((soup.find("div", {"class": "col-md-11"}).text).find(" 0 מסמכים לפרמטרים")!=-1):
+
+
+        try:
+            if((soup.find("div", {"class": "col-md-11"}).text).find(" 0 מסמכים לפרמטרים")!=-1):
+                if (counter > Years_and_Nums[year]):
                     STOP += 1
                     print("STOP ++")
-                    curr_case["תיק נמצא"] = False
-                    continue
-            except AttributeError:
-                pass
+                curr_case["תיק נמצא"] = False
 
+                continue
+        except AttributeError:
+            pass
+        STOP = 0
         curr_case["תיק נמצא"] = True
-
     # At least 2 secs must be waiting in order to load the webpage
         cont = 0
         while 0 <= cont < 5:
@@ -111,6 +110,7 @@ for year in Years_and_Nums.keys(): # CURR -> 2011 ONLY
             except TimeoutException:
                 if cont == 4:
                     print("Loading took too much time! ID in the code not working!")
+                    curr_case["תיק נמצא"] = False
                     break
                 print("Loading took too much time \nerror number ", cont+1, "\nTrying once again ...")
                 cont += 1
@@ -125,19 +125,20 @@ for year in Years_and_Nums.keys(): # CURR -> 2011 ONLY
             for I in range(3):
                 try:
                     dec_df, LINK, conclusion, dict = Crawl_Data.Crawl_Decisions(driver, Case_Link)  # Gets the button window
-                    curr_case["ניסיון להורדת מסמך"] = True
+                    curr_case["ניסיון להורדת ההחלטות"] = True
                 except OSError as error:
                     print("OS Error, on Crawl_Decisions, error num:", I + 1)
-                    curr_case["הצלחה בהורדת מסמך"] = False
+                    curr_case["הצלחה בהורדת ההחלטות"] = False
                     print(error)
                     continue
                 if (len(dec_df) > 0): break
             if (len(dec_df) == 0):
                 print("0 DEC!!!\n", Case_Link)
                 driver.get(Case_Link)
-                curr_case["הצלחה בהורדת מסמך"] = False
+                curr_case["הצלחה בהורדת ההחלטות"] = False
                 continue
-            curr_case["הצלחה בהורדת מסמך"] = True
+
+            curr_case["הצלחה בהורדת ההחלטות"] = True
             curr_case["סוג מסמך"] = conclusion
             print("Time until now current case (Crawl_Decisions) is: ", datetime.now() - START_CURR_TIME)
 
@@ -152,10 +153,24 @@ for year in Years_and_Nums.keys(): # CURR -> 2011 ONLY
             except OSError as error:
                 print("OS Error, on CrawlTopWindow, error num:", I + 1)
                 print(error)
-                data = Crawl_Data.CrawlTopWindow(Case_Link, LINK, conclusion, dict)
-            if (len(data) < 2):
-                print("MAIN GOT LEN LESS THAN 2!!!!!!!")
+                try:
+                    data = Crawl_Data.CrawlTopWindow(Case_Link, LINK, conclusion, dict)
+                except OSError as error:
+                    print("OS Error, on CrawlTopWindow, error num:", I + 1)
+                    print(error)
+                    curr_case["הצלחה בהורדת מטא-דאטה"] = False
+                    data = 0
+            try:
+                if (len(data) < 2):
+                    print("MAIN GOT LEN LESS THAN 2!!!!!!!")
+                    curr_case["הצלחה בהורדת מטא-דאטה"] = False
+                    continue
+            except:
+
+                print("error while crawling top window")
+
                 curr_case["הצלחה בהורדת מטא-דאטה"] = False
+                continue
 
             #######
             curr_case["הצלחה בהורדת מטא-דאטה"] = True
@@ -172,9 +187,12 @@ for year in Years_and_Nums.keys(): # CURR -> 2011 ONLY
             print("KEY ERROR")
 
         except UnexpectedAlertPresentException:
+
             counter -= 1
             print("UnexpectedAlertPresentException, continue")
+            curr_case["הצלחה בהורדת מטא-דאטה"] = False
             continue
+
         json_name = str(year) + "__" + str(counter)
         data['פרטי תיק']['שם הקובץ'] = json_name
         Save_As_Json.writeToJsonFile(filePath, json_name, data)  # Write to Json file
@@ -183,28 +201,18 @@ for year in Years_and_Nums.keys(): # CURR -> 2011 ONLY
               datetime.now() - START_CURR_TIME)
 
 
-
         counter += 1
         df = pd.DataFrame(curr_case, index=[0])
 
-        # for case_log in Logs_list:
-        #     # append data frame to CSV file
-        #     df.to_csv('Logs_DF.csv', mode='a', index=False, header=False)
-
         Logs_list.append(curr_case)
-        # for k,v in curr_case.items():
-        #     print(k, ": ",v)
-        # if counter == 3: break #############################################
-
-
 
 
     # Full_Log_Dict[year] = {"................."}
-
-Logs_DF = pd.DataFrame(columns=Logs_list[0].keys())
-Logs_DF.to_csv("Full_Logs_DF.csv")
+df.to_csv('Full_Logs_DF.csv', mode='a', index=False, header=False)
 
 
-for case_log in Logs_list:
-    # append data frame to CSV file
-    df.to_csv('Logs_DF.csv', mode='a', index=False, header=False)
+# Logs_DF = pd.DataFrame(columns=Logs_list[0].keys())
+# Logs_DF.to_csv("Full_Logs_DF.csv")
+
+
+
